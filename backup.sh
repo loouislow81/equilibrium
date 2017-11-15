fd259bbc666834b60f9335c28ba7d2d41b3e8fd7
Loouis Low <loouis@dogsbark.net>
2017-11-15 12:43:19 +0800

    indentation

--- backup.sh
+++ backup.sh
@@ -26,52 +26,65 @@ LOG="/home/loouis/.log/backup.log"
 
 ##runas root
 function root_only () {
-   if [ "$(whoami &2>/dev/null)" != "root" ] && [ "$(id -un &2>/dev/null)" != "root" ]
-      then
-         echo "[backup] Permission denied."
-         exit 1
-   fi
+  if [ "$(whoami &2> /dev/null)" != "root" ] &&
+     [ "$(id -un &2> /dev/null)" != "root" ]
+  then
+    echo "[backup] permission denied."
+    exit 1
+  fi
 }
 
 ### run equilibrium
 function run_backup () {
-   echo "[backup] started..."
-   equilibrium ${BACKUP_SOURCE} ${BACKUP_DESTINATION}
-   echo "[backup] done!"
+  echo "[backup] started..."
+  equilibrium ${BACKUP_SOURCE} ${BACKUP_DESTINATION}
+  echo "[backup] done!"
 }
 
 ### remove unwanted folders
 function optimize_backup () {
-   echo "[backup] optimizing backup..."
+  echo "[backup] optimizing backup..."
 
-   find ${BACKUP_DIR}/{$DEST_DIR} -name ${FLTR_FOLDER_1} -type d -exec rm -r {} \;
-   echo "[backup] FOLDER: Downloads removed!"
+  find ${BACKUP_DIR}/{$DEST_DIR} \
+    -name ${FLTR_FOLDER_1} \
+    -type d -exec rm -r {} \;
+  echo "[backup] FOLDER: Downloads removed!"
 
-   find ${BACKUP_DIR}/{$DEST_DIR} -name ${FLTR_FOLDER_2} -type d -exec rm -r {} \;
-   echo "[backup] FOLDER: Musics removed!"
+  find ${BACKUP_DIR}/{$DEST_DIR} \
+    -name ${FLTR_FOLDER_2} \
+    -type d -exec rm -r {} \;
+  echo "[backup] FOLDER: Musics removed!"
 
-   find ${BACKUP_DIR}/{$DEST_DIR} -name ${FLTR_FOLDER_3} -type d -exec rm -r {} \;
-   echo "[backup] FOLDER: Videos removed!"
+  find ${BACKUP_DIR}/{$DEST_DIR} \
+    -name ${FLTR_FOLDER_3} \
+    -type d -exec rm -r {} \;
+  echo "[backup] FOLDER: Videos removed!"
 
-   find ${BACKUP_DIR}/{$DEST_DIR} -name ${FLTR_FOLDER_4} -type d -exec rm -r {} \;
-   echo "[backup] FOLDER: Pictures removed!"
+  find ${BACKUP_DIR}/{$DEST_DIR} \
+    -name ${FLTR_FOLDER_4} \
+    -type d -exec rm -r {} \;
+  echo "[backup] FOLDER: Pictures removed!"
 
-   find ${BACKUP_DIR}/{$DEST_DIR} -name ${FLTR_FOLDER_5} -type d -exec rm -r {} \;
-   echo "[backup] FOLDER: log removed!"
+  find ${BACKUP_DIR}/{$DEST_DIR} \
+    -name ${FLTR_FOLDER_5} \
+    -type d -exec rm -r {} \;
+  echo "[backup] FOLDER: log removed!"
 
-   find ${BACKUP_DIR}/{$DEST_DIR} -name ${FLTR_FOLDER_6} -type d -exec rm -r {} \;
-   echo "[backup] FOLDER: Public removed!"
+  find ${BACKUP_DIR}/{$DEST_DIR} \
+    -name ${FLTR_FOLDER_6} \
+    -type d -exec rm -r {} \;
+  echo "[backup] FOLDER: Public removed!"
 }
 
 ### display total backup size
 function show_backup_size () {
-   echo "[backup] getting backup size..."
-   df ${BACKUP_SIZE}
+  echo "[backup] getting backup size..."
+  df ${BACKUP_SIZE}
 }
 
 ### write log
 function write_log () {
-   echo "[backup] sync finished at" `date` >> ${LOG}
+  echo "[backup] sync finished at" `date` >> ${LOG}
 }
 
 ### initialize
